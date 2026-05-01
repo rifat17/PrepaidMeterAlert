e9407c50a31f086d2ee74a1cd2eaf6ee1bf989dd
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2026-05-01 17:49:01 +0600

    feat: logger prints keys from ctx

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"os"
 )
@@ -12,16 +13,47 @@ const (
 	FormatJSON Format = "json"
 )
 
+type ctxKey struct{}
+
+// WithAttrs returns a context carrying slog attributes that will be injected
+// into every log line using that context.
+func WithAttrs(ctx context.Context, attrs ...slog.Attr) context.Context {
+	return context.WithValue(ctx, ctxKey{}, attrs)
+}
+
+type contextHandler struct {
+	inner slog.Handler
+}
+
+func (h *contextHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.inner.Enabled(ctx, level)
+}
+
+func (h *contextHandler) Handle(ctx context.Context, r slog.Record) error {
+	if attrs, ok := ctx.Value(ctxKey{}).([]slog.Attr); ok {
+		r.AddAttrs(attrs...)
+	}
+	return h.inner.Handle(ctx, r)
+}
+
+func (h *contextHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &contextHandler{inner: h.inner.WithAttrs(attrs)}
+}
+
+func (h *contextHandler) WithGroup(name string) slog.Handler {
+	return &contextHandler{inner: h.inner.WithGroup(name)}
+}
+
 // InitLogger configures the default slog logger with the given level and output format.
 func InitLogger(level slog.Level, format Format) {
 	opts := &slog.HandlerOptions{Level: level}
 
-	var handler slog.Handler
+	var inner slog.Handler
 	if format == FormatJSON {
-		handler = slog.NewJSONHandler(os.Stderr, opts)
+		inner = slog.NewJSONHandler(os.Stderr, opts)
 	} else {
-		handler = slog.NewTextHandler(os.Stderr, opts)
+		inner = slog.NewTextHandler(os.Stderr, opts)
 	}
 
-	slog.SetDefault(slog.New(handler))
+	slog.SetDefault(slog.New(&contextHandler{inner: inner}))
 }
