4910234692b73eb9862ae947eb4e821ae3f770c6
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2026-05-08 00:58:42 +0600

    feat: default model with bun orm

---
+++ internal/database/models/default.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+type BaseTimeStampedModel struct {
+	bun.BaseModel
+
+	ID        uuid.UUID  `bun:"id,pk,type:uuid,default:gen_random_uuid_v7()"`
+	CreatedAt *time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt *time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
+	DeletedAt *time.Time `bun:"deleted_at,soft_delete,nullzero"`
+}
+
+func (m *BaseTimeStampedModel) BeforeAppendModel(ctx context.Context, query bun.Query) error {
+	now := time.Now()
+
+	switch query.(type) {
+	case *bun.InsertQuery:
+		m.CreatedAt = &now
+		m.UpdatedAt = &now
+	case *bun.UpdateQuery:
+		m.UpdatedAt = &now
+	}
+
+	return nil
+}
