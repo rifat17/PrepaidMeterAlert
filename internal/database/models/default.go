adc39cd6d75db60d83b3c5d956a7767a019d4c1b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2026-05-08 01:13:28 +0600

    fix: sql func for uuidv7

--- internal/database/models/default.go
+++ internal/database/models/default.go
@@ -11,7 +11,7 @@ import (
 type BaseTimeStampedModel struct {
 	bun.BaseModel
 
-	ID        uuid.UUID  `bun:"id,pk,type:uuid,default:gen_random_uuid_v7()"`
+	ID        uuid.UUID  `bun:"id,pk,type:uuid,default:uuidv7()"`
 	CreatedAt *time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
 	UpdatedAt *time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
 	DeletedAt *time.Time `bun:"deleted_at,soft_delete,nullzero"`
